26a143d6d032e32d19141fa7c761e5c6cb2aaf45
JangHayoung <1010hy@naver.com>
2024-01-15 21:07:15 +0900

    241115_ch07_2

--- Ch08/src/ch07/Fibonacci.java
+++ Ch08/src/ch07/Fibonacci.java
@@ -10,19 +10,22 @@ public class Fibonacci {
 		value = new int[number];
 	}
 	
+	//1. 재귀 함수로 풀이
 	public int fibonacciRecur(int n) {
 		
 
 		if (n == 0) return 0;
 		if (n == 1) return 1;
 
+		//값을 함수로 호출하는 형태이다.
 		return fibonacciRecur(n - 1) + fibonacciRecur(n - 2);
 	}
 	
+	//2. 반복문으로 풀이
 	public int fibonacciIter(int n) {
 		
-		int ppre = 0;
-		int pre = 1;
+		int ppre = 0;	//전전 값
+		int pre = 1;	//전 값
 		int current = 0;
 
 		if (n == 0) return 0;
@@ -38,6 +41,7 @@ public class Fibonacci {
 		return current;
 	}
 	
+	//3. 메모이제이션, value배열에 넣는 방법
 	public int fibonacciMem(int n) {
 		
 		value[0] = 0;
@@ -65,9 +69,10 @@ public class Fibonacci {
 		return result;
 	}
 	
-	
+	//실행코드
 	public static void main(String[] args) {
 
+		//배열 수를 잡기 위해 100을 넣었다.
 		Fibonacci fib = new Fibonacci(100);
 		
 		int result = fib.fibonacciRecur(10);
