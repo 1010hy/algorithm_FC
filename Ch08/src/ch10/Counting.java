bbdc636a17be8ce908ff1cd737d05ae3fa113ab0
JangHayoung <1010hy@naver.com>
2024-01-17 15:35:10 +0900

    240117_ch10_2

--- Ch08/src/ch10/Counting.java
+++ Ch08/src/ch10/Counting.java
@@ -7,7 +7,6 @@ public class Counting {
 		int[] people = { 55, 40, 27, 99, 76, 81, 29, 31,33, 62}; 
 		int[] ages = new int[10]; //연령대에 따른 수 세기
 		
-
 		for(int i = 0; i<people.length; i++) {
 			int age = people[i];
 			if(age <30) ages[0]++;
@@ -20,16 +19,19 @@ public class Counting {
 			else if(age <= 100) ages[7]++;
 		}
 		int number = people.length;
-		System.out.println( number + "명 중 20대는 " + ages[0]+ "명 입니다.");
-		System.out.println( number + "명 중 30대는 " + ages[1]+ "명 입니다.");
-		System.out.println( number + "명 중 40대는 " + ages[2]+ "명 입니다.");
-		System.out.println( number + "명 중 50대는 " + ages[3]+ "명 입니다.");
-		System.out.println( number + "명 중 60대는 " + ages[4]+ "명 입니다.");
-		System.out.println( number + "명 중 70대는 " + ages[5]+ "명 입니다.");
-		System.out.println( number + "명 중 80대는 " + ages[6]+ "명 입니다.");
-		System.out.println( number + "명 중 90대는 " + ages[7]+ "명 입니다.");
 		
+		for(int i = 0; i<8; i++) {
+			System.out.println( number + "명 중 " + (i+2)+"0대는 " + ages[i]+ "명 입니다.");
+		}
+		
+//		System.out.println( number + "명 중 20대는 " + ages[0]+ "명 입니다.");
+//		System.out.println( number + "명 중 30대는 " + ages[1]+ "명 입니다.");
+//		System.out.println( number + "명 중 40대는 " + ages[2]+ "명 입니다.");
+//		System.out.println( number + "명 중 50대는 " + ages[3]+ "명 입니다.");
+//		System.out.println( number + "명 중 60대는 " + ages[4]+ "명 입니다.");
+//		System.out.println( number + "명 중 70대는 " + ages[5]+ "명 입니다.");
+//		System.out.println( number + "명 중 80대는 " + ages[6]+ "명 입니다.");
+//		System.out.println( number + "명 중 90대는 " + ages[7]+ "명 입니다.");
 		
 	}
-
 }
